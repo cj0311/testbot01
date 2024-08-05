ed931700a86e6c96d110dabb84f9149fb682e081
cj0311 <quritis@naver.com>
2024-08-05 22:12:44 +0900

    first commit

---
+++ src/main/java/com/tradingview/testbot01/TestBot01Application.java
@@ -0,0 +1,13 @@
+package com.tradingview.testbot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBot01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBot01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tradingview/testbot01/TestBot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradingview.testbot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBot01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
